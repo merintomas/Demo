e1b36ef4be54b5237b5da72a494ae3d4c9db1ccb
merintomas <112062281+merintomas@users.noreply.github.com>
2022-10-06 11:56:23 +0530

    Add files via upload

---
+++ CheckTitle.java
@@ -0,0 +1,30 @@
+package Assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class CheckTitle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String actualTitle=driver.getTitle();
+		if(actualTitle.equals("Amazon"))
+		{
+			System.out.println("Titles are  matching");
+		}
+		else
+		{
+			System.out.println("titles are not matching");
+		}
+	
+	driver.close();
+	}
+}
